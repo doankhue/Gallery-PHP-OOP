c6606c2f07fd675c28972707ab67f4a345fa343b
Doan Khue <doankhue09@gmail.com>
2019-04-10 22:55:08 +0700

    create function login

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -17,7 +17,14 @@ class Session
 	public function is_signed_in(){
 		return $this->signed_in;
 	}
-	
+
+	public function login($user){
+		if($user){
+			$this->user_id = $_SESSION['user_id'] = $user->id;
+			$this->signed_in = true;
+		}
+	}
+
 	private function check_the_login(){
 		if (isset($_SESSION['user_id'])) {
 			$this->user_id = $_SESSION['user_id'];
