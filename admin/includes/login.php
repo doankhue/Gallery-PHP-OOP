1f3c288e97192e6677a33e4b3d95e0bbd6bc9b33
Doan Khue <doankhue09@gmail.com>
2019-04-10 23:38:40 +0700

    create login file

---
+++ admin/includes/login.php
@@ -0,0 +1,27 @@
+<?php  
+
+require_once('init.php');
+
+if($sessiton->is_signed_in()){
+	redirect("index.php")
+}
+
+if(isset($_POST['submit'])){
+
+	$username = trim($_POST['username']);
+
+	$password = trim($_POST['password']);
+
+	if($user_found){
+		$sessiton->login($user_found);
+		redirect("index.php");
+	}else{
+		$the_message = "Your password or username are incorrect";
+	}
+
+}else{
+	$username = "";
+	$password = "";
+}
+
+?>
